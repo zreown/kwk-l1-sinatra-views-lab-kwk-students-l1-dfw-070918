d20e938bb2a3e22d266adabb60bbef5d84d9fc49
Ian Candy <iancandysemail@gmail.com>
2015-09-15 16:20:59 -0400

    remove solution

--- app.rb
+++ app.rb
@@ -6,23 +6,5 @@ class App < Sinatra::Base
 		erb :index
 	end
 
-	get '/hello' do
-		erb :hello
-	end
-
-	get '/goodbye' do 
-		erb :goodbye
-	end
-
-	get '/dogs' do
-		erb :dogs
-	end
-
-	get '/cats' do
-		erb :cats
-	end
-
-	get '/music' do
-		erb :music
-	end
+	
 end
