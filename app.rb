defeb218ffc5c0dde80b0c396660315f5284dc0f
aviflombaum <avi@flombaum.com>
2015-09-15 22:50:25 -0400

    Solution

--- app.rb
+++ app.rb
@@ -4,4 +4,24 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/cats' do
+		erb :cats
+	end
+
+	get '/dogs' do
+		erb :dogs
+	end
+
+	get '/music' do
+		erb :music
+	end
+
 end
